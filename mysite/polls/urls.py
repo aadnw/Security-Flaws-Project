450b412611f41d65cefd409fac76cfb53c9e1242
Aada Nwajei <aada.nwajei@gmail.com>
2025-05-28 11:36:54 +0300

    Add url to the vulnerable page

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("demonstrate-sql-injection/", views.demonstrate_sql_injection, name="demonstrate_sql_injection")
 ]
